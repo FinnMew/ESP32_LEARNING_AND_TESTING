fdfe5329eef04a61aa612e8c4fb9f3d95db870d7
FinnMew <finngrayharvey@outlook.com>
2024-06-08 20:41:24 +1000

    feature signup working

--- src/main.c
+++ src/main.c
@@ -5,8 +5,21 @@
 #include <soc/timer_periph.h>
 #include <driver/gptimer.h>
 #include <driver/gpio.h>
+#include "esp_log.h"
 
 #define pinLED 6
+#define TAG "MAIN"
+
+static void IRAM_ATTR timer_isr(void* arg) {
+
+    REG_WRITE(TIMG_INT_CLR_TIMERS_REG(0), TIMG_T0_INT_CLR) ;
+
+    static bool led_on = false;
+    led_on = !led_on;
+    gpio_set_level(pinLED, led_on);
+
+    REG_SET_BIT(TIMG_T0CONFIG_REG(0), TIMG_T0_ALARM_EN) ;
+}
 
 void app_main() {
 
@@ -22,32 +35,25 @@ void app_main() {
     REG_WRITE(TIMG_T0LOAD_REG(0), 0); 
     REG_SET_BIT(TIMG_T0CONFIG_REG(0), TIMG_T0_EN);
 
+    uint32_t start_alarm_low = 1220;  
+    uint32_t start_alarm_high = 0; 
+    REG_WRITE(TIMG_T0ALARMLO_REG(0), start_alarm_low) ;
+    REG_WRITE(TIMG_T0ALARMHI_REG(0), start_alarm_high) ;
+    REG_SET_BIT(TIMG_INT_ENA_TIMERS_REG(0), TIMG_T0_INT_ENA) ;
+    REG_SET_BIT(TIMG_T0CONFIG_REG(0), TIMG_T0_AUTORELOAD);
+
+    uint32_t reload_value_low = 0;  
+    uint32_t reload_value_high = 0; 
+    REG_WRITE(TIMG_T0LOADLO_REG(0), reload_value_low) ;
+    REG_WRITE(TIMG_T0LOADHI_REG(0), reload_value_high) ;
+    REG_SET_BIT(TIMG_T0CONFIG_REG(0), TIMG_T0_ALARM_EN);
+
+    esp_intr_alloc(ETS_TG0_T0_LEVEL_INTR_SOURCE, 0, timer_isr, NULL, NULL) ;
+
+
     while(1) {
-
-        REG_WRITE(TIMG_T0UPDATE_REG(0), 1) ;
-        while (REG_READ(TIMG_T0UPDATE_REG(0)) != 0) ;
-
-        uint32_t timer_value_low = REG_READ(TIMG_T0LO_REG(0));
-        uint32_t timer_value_high = REG_READ(TIMG_T0HI_REG(0));
-        uint64_t timer_value = ((uint64_t)timer_value_high << 32) | timer_value_low;
-  
-
-    if(timer_value >= 1220) {
-            gpio_set_level(pinLED, 1) ;
-    if(timer_value >= 2441) {
-        gpio_set_level(pinLED, 0) ;
-        REG_CLR_BIT(TIMG_T0CONFIG_REG(0), TIMG_T0_EN);
-        REG_WRITE(TIMG_T0LOADLO_REG(0), start_value_low);
-        REG_WRITE(TIMG_T0LOADHI_REG(0), start_value_high);
-        REG_WRITE(TIMG_T0LOAD_REG(0), 0); 
-        REG_SET_BIT(TIMG_T0CONFIG_REG(0), TIMG_T0_EN);
-
-    }
-    }
-
-    
-
-
+          
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
 
